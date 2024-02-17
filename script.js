32f57aee863cdbe1a55251fda7412db71881a4a7
SkYfy1 <festik.tawer@gmail.com>
2024-02-18 00:42:46 +0200

    added storage

--- script.js
+++ script.js
@@ -2,18 +2,38 @@ const inputBox = document.getElementById('input');
 const listContainer = document.getElementById('listcontainer');
 const button = document.querySelector('button');
 
+const existingTodos = JSON.parse(localStorage.getItem('todos'));
+
+let todoData = [];
+
+todoData = existingTodos || [];
+
+todoData.forEach(element => {
+        let li = document.createElement('li');
+        li.innerHTML = element;
+        listContainer.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+});
+
+
+
 function addTask(){
     if (inputBox.value === '') {
         alert('You must write something!');
     } else {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
+        todoData.push(inputBox.value);
         listContainer.appendChild(li);
 
         let span = document.createElement('span');
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
+    localStorage.setItem('todos', JSON.stringify(todoData));
 
     inputBox.value = '';
 };
